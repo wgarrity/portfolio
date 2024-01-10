b255f510510d31f5642d2852cc6274b8bdda6b21
william garrity <garrity.aiq@gmail.com>
2024-01-10 14:40:38 -0500

    header and title card added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './styles/styles.scss';
 import logo from './logo.svg';
 import './App.css';
+import { Header, TitleCard } from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/ >
+      <section>
+        <TitleCard />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      </section>
     </div>
   );
 }
---
+++ src/components/TitleCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TitleCard = () => {
+    return (
+        <titleCard>
+            <h1>William Garrity</h1>
+            <span>Design Thinker, User Advocate, UX Enthusiast</span>
+        </titleCard>
+    );
+};
+
+export default TitleCard;
